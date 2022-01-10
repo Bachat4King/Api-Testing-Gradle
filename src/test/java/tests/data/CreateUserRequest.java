fece6750aea3011937094507d77b23c4980be667
Bastian Alejandro Silva Fuenzalida <bastian.silva@globant.com>
2022-01-10 04:19:34 -0300

    [+] adding users classes

---
+++ src/test/java/tests/data/CreateUserRequest.java
@@ -0,0 +1,25 @@
+package tests.data;
+
+public class CreateUserRequest {
+	private String password;
+	private String email;
+
+	public CreateUserRequest(){
+	}
+
+	public void setPassword(String password){
+		this.password = password;
+	}
+
+	public String getPassword(){
+		return password;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+}
