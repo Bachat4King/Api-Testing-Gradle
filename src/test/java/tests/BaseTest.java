7f457bb8cc64f0e15b433d347559dae0c6d26ad0
Bastian Alejandro Silva Fuenzalida <bastian.silva@globant.com>
2022-01-10 04:17:58 -0300

    [+] adding base class

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeClass;
+
+
+
+public abstract class BaseTest {
+
+    private static final Logger logger = LogManager.getLogger(ReqResTests.class);
+
+    @BeforeClass
+    public static void setUp() {
+        logger.info("Setting up the base test");
+        RestAssured.requestSpecification = defaultRequestSpecification();
+        logger.info("Base test setup complete");
+    }
+
+    public static RequestSpecification defaultRequestSpecification() {
+        return new RequestSpecBuilder().setBaseUri("https://reqres.in")
+                .setBasePath("api/")
+                .setContentType(ContentType.JSON).build();
+    }
+
+}
