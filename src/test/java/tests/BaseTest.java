708fd046146d5cbfd8abcc055f86412cd04d50a2
Bastian Alejandro Silva Fuenzalida <bastian.silva@globant.com>
2022-02-21 15:15:21 -0300

    [+] adding necesary files and modified files

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,12 +2,20 @@ package tests;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.BeforeClass;
 import utils.ConfVariables;
+import io.restassured.filter.Filter;
+import io.qameta.allure.restassured.AllureRestAssured;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public abstract class BaseTest {
@@ -22,6 +30,11 @@ public abstract class BaseTest {
     }
 
     public static RequestSpecification defaultRequestSpecification() {
+        List<Filter> filters = new ArrayList<>();
+        filters.add(new RequestLoggingFilter());
+        filters.add(new ResponseLoggingFilter());
+        filters.add(new AllureRestAssured());
+
         return new RequestSpecBuilder().setBaseUri(ConfVariables.getHost())
                 .setBasePath(ConfVariables.getPath())
                 .setContentType(ContentType.JSON).build();
